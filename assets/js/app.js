8bd8be135883345e1970e455c5c840099358ba44
Duc Dang <dangminhduca3@gmail.com>
2022-12-30 23:50:32 +0700

    create top header component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,17 @@
 import headerComponent from "./modules/components/headerComponent.js";
+import TopHeader from "./modules/components/topheaderComponent.js";
 import RESTfulAPI from "./modules/client/RESTfulAPI.js";
 const app = {
   $: document.querySelector.bind(document),
   $$: document.querySelectorAll.bind(document),
   header: function () {
     const getComponent = this.$("headerComponent");
+    const topHeaders = this.$$('topHeader');
+    topHeaders.forEach((topHeader, index) => {
+      const topHeaderElm = new TopHeader(topHeader, index);
+      topHeaderElm.create();
+      topHeaderElm.action();
+    });
     let wrapElement = this.$(".wrap");
     let header = new headerComponent(getComponent, wrapElement);
     header.create;
--- assets/js/modules/components/topheaderComponent.js
+++ assets/js/modules/components/topheaderComponent.js
@@ -1,7 +1,42 @@
-class topHeader {
-    constructor(text, logo) {
-        this.text=text;
-        this.logo=logo
+
+class TopHeader {
+    constructor(component, idHeader) {
+        this.component = component;
+        this.idHeader = idHeader;
         }
-    creat
+    create() {
+        
+        const textHeader = this.component.getAttribute('text');
+        const color = this.component.getAttribute('color');
+        const route = this.component.getAttribute('route');
+        const headerEml = `
+          <div route="${route}" class="top-header top-header-elm-${this.idHeader}" style="color:${color}">
+            ${textHeader}
+          </div>`;
+        this.component.insertAdjacentHTML('afterend', headerEml)
+        this.component.remove();
+        
+    }
+
+    action() {
+      const currentElm = document.querySelector(`.top-header-elm-${this.idHeader}`);
+      currentElm.addEventListener('click', (e) => {
+        const route = e.target.getAttribute('route') 
+        window.history.pushState({}, '', route);
+        this.handleLocation()
+      })
+    }
+
+    handleLocation() {
+      const currentRoute = window.location.pathname;
+      if (currentRoute === '/laptop') {
+        console.log('abc');
+        const h1 = document.createElement('h1')
+        h1.textContent = 'hello'
+        document.body.appendChild(h1)
+      }
+    }
 }
+
+
+export default TopHeader
